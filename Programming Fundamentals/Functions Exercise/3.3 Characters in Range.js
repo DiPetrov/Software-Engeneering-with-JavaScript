8bb5195ed70234262a6729b8c9432fd4a97032a1
Dimitar Petrov <dimitarpetrov_de@yahoo.com>
2025-07-26 10:34:52 +0200

    New Solution for Task 3 Characters in Range

---
+++ Programming Fundamentals/Functions Exercise/3.3 Characters in Range.js
@@ -0,0 +1,26 @@
+function charactersInRange(a, b) {
+    let result = " ";
+
+    function printASCII(x, y) {
+        let char1 = x.charCodeAt(0);
+        let char2 = y.charCodeAt(0);
+
+        if (char1 > char2) {
+            for (let i = char2 + 1; i < char1; i++) {
+                result +=  String.fromCharCode(i) + " ";
+            }
+        } else {
+            for (let i = char1 + 1; i < char2; i++) {
+                result += String.fromCharCode(i) + " ";
+
+            }
+        }
+    }
+
+    printASCII(a, b);
+    console.log(result);
+}
+
+// charactersInRange('a', 'd');
+// charactersInRange('#', ':');
+charactersInRange('C', '#');
