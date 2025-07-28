dba8989d3b62017211e1b18aa6040536af9e3e7a
Dimitar Petrov <dimitarpetrov_de@yahoo.com>
2025-07-28 11:38:02 +0200

    New Task 5 - Sorting an array by 2 criteria from Array Advanced Exercise

---
+++ Programming Fundamentals/Array Advanced Exercise/5. Sort an array by Criteria.js
@@ -0,0 +1,11 @@
+function sortArrayBy2Criteria(arr) {
+    arr.sort((a, b) => {
+        if (a.length !== b.length) {
+            return a.length - b.length;
+        }
+        return a.toLowerCase().localeCompare(b.toLocaleLowerCase());
+    });
+    console.log(arr.join('\n'));
+}
+
+sortArrayBy2Criteria(['alpha', 'beta', 'gamma']);
