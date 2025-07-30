5f983fe4df9a52c7c762464b808324fad9a61023
Dimitar Petrov <dimitarpetrov_de@yahoo.com>
2025-07-30 07:36:10 +0200

    Solution for  Tasks 5 from Objects and Classes Lab

---
+++ Programming Fundamentals/Objects and Classes Lab/5. Cats.js
@@ -0,0 +1,25 @@
+function cats(catDataArr) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        meow() {
+            return `${this.name}, age ${this.age} says Meow`;
+        }
+    }
+
+    for (let catData of catDataArr) {
+    let parsedData = catData.split(' ');
+
+    let catName = parsedData[0];
+    let catAge = parsedData[1];
+
+    let cat = new Cat(catName, catAge);
+
+    console.log(cat.meow());
+}
+}
+
+cats(['Mellow 2', 'Tom 5']);
