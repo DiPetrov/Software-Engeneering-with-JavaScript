306f8bdc77ac77a42653b036db28944d6bc84644
Dimitar Petrov <dimitarpetrov_de@yahoo.com>
2025-08-07 10:42:06 +0200

    Solution for Task 3 from Objects and Classes Exercise

---
+++ Programming Fundamentals/Objcts and Classes Exercise/3. Store Provisions.js
@@ -0,0 +1,28 @@
+function storeProvisions(currentStock, deliveryStock) {
+    let store = {};
+
+    for (let i = 0; i < currentStock.length; i += 2) {
+        let product = currentStock[i];
+        let quantity = Number(currentStock[i + 1]);
+        store[product] = quantity;
+    }
+
+    for (let i = 0; i < deliveryStock.length; i += 2) {
+        let product = deliveryStock[i];
+        let quantity = Number(deliveryStock[i + 1]);
+        if (store.hasOwnProperty(product)) {
+            store[product] += quantity;
+        } else {
+            store[product] = quantity;
+        }
+    }
+
+    for (let product in store) {
+        console.log(`${product} -> ${store[product]}`);
+    }
+}
+
+storeProvisions(['Chips', '5', 'CocaCola', '9', 'Bananas',
+                 '14', 'Pasta', '4', 'Beer', '2'],
+                ['Flour', '44', 'Oil', '12', 'Pasta', '7',
+                'Tomatoes', '70', 'Bananas', '30']);
