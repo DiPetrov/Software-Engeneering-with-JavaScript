28e80c32473b82c19239ba08e0b855f1c4e10bc4
Dimitar Petrov <dimitarpetrov_de@yahoo.com>
2025-08-10 21:34:01 +0200

    Solution for Tasks from Objects and Classes

---
+++ Programming Fundamentals/Objcts and Classes Exercise/6. Make a Dictionary.js
@@ -0,0 +1,21 @@
+function makeDictionary(jsonArray) {
+    let dictionary = {};
+
+    for (let jsonStr of jsonArray) {
+        let entry = JSON.parse(jsonStr);
+        let term = Object.keys(entry)[0];
+        let definition = entry[term];
+        dictionary[term] = definition;
+    }
+    let sortedTerms = Object.keys(dictionary).sort();
+
+    for (let term of sortedTerms) {
+        console.log(`Term: ${term} => Definition: ${dictionary[term]}`);
+    }
+}
+
+makeDictionary(['{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of at tropical shrub."}',
+                '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+                '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+                '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+                '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}']);
