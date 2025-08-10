dcbbb50440c8572a73fe2e4416c421e009792b51
Dimitar Petrov <dimitarpetrov_de@yahoo.com>
2025-08-10 21:32:56 +0200

    Solution for Task 1 from RegeEx Lab

---
+++ Programming Fundamentals/Regular Expressions Lab/1. Match full name.js
@@ -0,0 +1,14 @@
+function matchFullname(input) {
+    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+
+    let results = input.matchAll(pattern);
+    let output = [];
+
+    for (let match of results) {
+        output.push(match[0]);
+    }
+
+    console.log(output.join(' '));
+}
+
+matchFullname("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan Ivanov");
